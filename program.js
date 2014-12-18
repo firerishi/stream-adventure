1348d575a78f8e53a5f903b2ca30f05f47dffd96
Srinivas, Thirumalaa <SrinivaT@advisory.com>
2014-12-18 15:22:53 -0600

    Lesson 4 completed on transforms.

--- program.js
+++ program.js
@@ -1,3 +1,14 @@
+// lesson #4
+// transform
+var through = require('through');
+var tr = through(write, end);
+
+function write (buf) { this.queue(buf.toString().toUpperCase())}
+function end () { this.queue(null) }
+
+process.stdin.pipe(tr).pipe(process.stdout);
+
+
 // lesson #3
 // input output
 process.stdin.pipe(process.stdout);
