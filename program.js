c80d8882080a9673dff0453ee3f2a935edb6afe4
Srinivas, Thirumalaa <SrinivaT@advisory.com>
2014-12-30 10:03:46 -0600

    Lesson 11 on duplexer solved.

--- program.js
+++ program.js
@@ -1,3 +1,14 @@
+// lesson #11
+// duplexer
+var spawn = require('child_process').spawn;
+var duplexer = require('duplexer');
+
+module.exports = function(cmd, args) {
+	var ps = spawn(cmd, args);
+	return duplexer(ps.stdin, ps.stdout);
+};
+
+
 // lesson #10
 // html stream
 var trumpet = require('trumpet');
