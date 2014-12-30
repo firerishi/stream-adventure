043673db0460318de9e788420af03103fc1e8eb4
Srinivas, Thirumalaa <SrinivaT@advisory.com>
2014-12-30 11:22:54 -0600

    Lesson #13 on combiner completed.

--- program.js
+++ program.js
@@ -1,3 +1,38 @@
+// lesson #13
+// combiner
+var combine = require('stream-combiner');
+var through = require('through');
+var split = require('split');
+var zlib = require('zlib');
+
+module.exports = function () {
+    var grouper = through(write, end);
+    var current;
+    
+    function write (line) {
+        if (line.length === 0) return;
+        var row = JSON.parse(line);
+        
+        if (row.type === 'genre') {
+            if (current) {
+                this.queue(JSON.stringify(current) + '\n');
+            }
+            current = { name: row.name, books: [] };
+        }
+        else if (row.type === 'book') {
+            current.books.push(row.name);
+        }
+    }
+    function end () {
+        if (current) {
+            this.queue(JSON.stringify(current) + '\n');
+        }
+        this.queue(null);
+    }
+    
+    return combine(split(), grouper, zlib.createGzip());
+};
+
 // lesson #12
 // duplexer redux
 var duplexer = require('duplexer');
